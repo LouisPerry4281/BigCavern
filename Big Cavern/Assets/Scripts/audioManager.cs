ff49dc350fa0e42ef6faab80f66e8a338c1f8932
Tom Prince <123157224+TPrincy@users.noreply.github.com>
2024-06-04 15:57:28 +0100

    Tom push has audio added into the scene

---
+++ Big Cavern/Assets/Scripts/audioManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class audioManager : MonoBehaviour
+{
+    public AudioSource soundTracks;
+        public AudioClip[] soundtracksArray;
+}
